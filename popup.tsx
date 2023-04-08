28ed9232c0915a026cf8866d742416721abec687
shenxin <363088847@qq.com>
2023-04-08 23:56:36 +0800

    init

--- popup.tsx
+++ popup.tsx
@@ -1,26 +1,14 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
-
+   useEffect(() => {
+    chrome.runtime.onMessage.addListener((event) => {
+      console.log(' chrome.runtime.onMessage.addListener', JSON.parse(event.detail))
+   });
+  }, [])
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div>
+      666
     </div>
   )
 }
